d780bca36e6395e8defc3cbf0748fa973f6c6fa1
Fritz Mbog <fritz-external@bluewindowltd.com>
2024-09-30 14:26:28 +0100

    tets

--- site/main.js
+++ site/main.js
@@ -27,7 +27,7 @@ addEventListener("fetch", (event) => {
 // A simple request handler function
 async function handleRequest(request) {
     // Example: Returning a simple HTML response
-    return new Response("Hello, Cloudflare Workers!", {
+    return new Response("Hello, Cloudflare Workers that good Fritzo!", {
         headers: { "Content-Type": "text/html" },
     });
 }
