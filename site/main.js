428ba3e69116aa657cc0588e0aa1d15549dad1f7
Fritz Mbog <fritz-external@bluewindowltd.com>
2024-09-30 14:13:21 +0100

    tets

--- site/main.js
+++ site/main.js
@@ -1,11 +1,24 @@
+// Function to display a notice (Note: This won't directly work in Cloudflare Workers)
 function displayNotice(index) {
-    document.getElementsByClassName("in-development")[index].style.display = "block";
+    // Cloudflare Workers do not have direct access to the DOM
+    // Therefore, this code should be handled on the client side (e.g., in your HTML or browser script)
+    if (typeof document !== "undefined") {
+        document.getElementsByClassName("in-development")[index].style.display = "block";
+    }
 }
 
-window.onload = function() {
-    new Vivus("ecosystem-graphic", {
-        file: "/images/simulators-graphic.svg",
-        duration: 400,
-        type: "oneByOne"
-    }).play();
-};
+// Check if the script is running in a browser environment
+if (typeof window !== "undefined") {
+    window.onload = function() {
+        // Initialize Vivus if in a browser environment
+        new Vivus("ecosystem-graphic", {
+            file: "/images/simulators-graphic.svg",
+            duration: 400,
+            type: "oneByOne"
+        }).play();
+    };
+} else {
+    // Cloudflare Workers context does not execute the above script
+    // You might want to add Worker-specific code here if needed
+    console.log("This script is running in a Cloudflare Worker context.");
+}
